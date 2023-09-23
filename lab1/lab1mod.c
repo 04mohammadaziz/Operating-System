e2bcd2ae363ae987fadab963af8b09745a0ed919
Mohammad <20maa2@queensu.ca>
2023-09-23 12:56:39 -0400

    Added problem decription solution, testing and result documents, and miscellaneous files

--- lab1/lab1mod.c
+++ lab1/lab1mod.c
@@ -1,57 +1,65 @@
+// Include necessary headers
 #include <linux/version.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
-
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 #include <linux/ktime.h>
 
-
+// Check for Linux Kernel version to define HAVE_PROC_OPS
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5,6,0)
 #define HAVE_PROC_OPS
 #endif
 
+// Function to show process information
 static int lab1_show(struct seq_file *m, void *v) {
-  /* some code here */
-  const struct cred*cred = current_cred();
+  // Get current process credentials
+  const struct cred *cred = current_cred();
 
+  // Print various attributes of the current process
   seq_printf(m, "Current Process PCB Information\n");
   seq_printf(m, "Name = %s\n", current->comm);
   seq_printf(m, "PID = %d\n", current->pid);
   seq_printf(m, "PPID = %d\n", task_ppid_nr(current));
-  seq_printf(m, "State: \n");
-  switch (current->state)
-  {
-  case 0:
-    seq_printf(m, "TASK_RUNNING\n");
-    seq_printf(m, "Running...");
-    break;
-  case 1:
-    seq_printf(m, "TASK INTERRUPTIBLE");
-    seq_printf(m, "TASK_STOPPED");
-    seq_printf(m, "Stopped...");
-    break;
-  default:
-    seq_printf(m, "Waiting...");
-    break;
+  seq_printf(m, "State: ");
+
+  // Print the state of the process
+  switch (current->state) {
+    case TASK_RUNNING:
+      seq_printf(m, "Running\n");
+      break;
+    case TASK_INTERRUPTIBLE:
+    case TASK_UNINTERRUPTIBLE:
+      seq_printf(m, "Waiting\n");
+      break;
+    case TASK_STOPPED:
+      seq_printf(m, "Stopped\n");
+      break;
+    default:
+      seq_printf(m, "Unknown\n");
+      break;
   }
-  seq_printf(m, "Real UID = %d\n", cred->uid.val);  
+
+  // Print UID and GID information
+  seq_printf(m, "Real UID = %d\n", cred->uid.val);
   seq_printf(m, "Effective UID = %d\n", cred->euid.val);
   seq_printf(m, "Saved UID = %d\n", cred->suid.val);
   seq_printf(m, "Real GID = %d\n", cred->gid.val);
   seq_printf(m, "Effective GID = %d\n", cred->egid.val);
   seq_printf(m, "Saved GID = %d\n", cred->sgid.val);
+
   return 0;
 }
 
+// Function to open the proc file
 static int lab1_open(struct inode *inode, struct  file *file) {
   return single_open(file, lab1_show, NULL);
 }
 
+// Define file operations
 #ifdef HAVE_PROC_OPS
 static const struct proc_ops lab1_fops = {
-  /* operation mapping */
   .proc_open = lab1_open,
   .proc_read = seq_read,
   .proc_lseek = seq_lseek,
@@ -59,7 +67,6 @@ static const struct proc_ops lab1_fops = {
 };
 #else
 static const struct file_operations lab1_fops = {
-  /* operation mapping */
   .open = lab1_open,
   .read = seq_read,
   .llseek = seq_lseek,
@@ -67,22 +74,25 @@ static const struct file_operations lab1_fops = {
 };
 #endif
 
+// Initialization function
 static int __init lab1_init(void) {
-  /* create proc entry */
+  // Create a new proc entry
   proc_create("lab1", 0, NULL, &lab1_fops) == NULL;
-    if (!proc_create) {
-      return -ENOMEM;
-    }
+  if (!proc_create) {
+    return -ENOMEM;
+  }
   printk(KERN_INFO "lab1mod in\n");
   return 0;
 }
 
+// Cleanup function
 static void __exit lab1_exit(void) {
-  /* remove proc entry */
+  // Remove the proc entry
   remove_proc_entry("lab1", NULL);
   printk(KERN_INFO "lab1mod out\n");
 }
 
+// Module information
 MODULE_LICENSE("GPL");
 module_init(lab1_init);
 module_exit(lab1_exit);
---
+++ lab1/lab1mod.mod
@@ -0,0 +1,2 @@
+/home/20maa2/elec377-tues-pm-60/lab1/lab1mod.o
+
