418b12b38340b0fd45a7bdb7f0297c3642b5d07f
Mohammad <20maa2@queensu.ca>
2023-10-08 13:55:59 -0400

    Updated lab2

--- lab2/shell.c
+++ lab2/shell.c
@@ -1,3 +1,4 @@
+// Additional libraries imported for various functionalities
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -8,6 +9,7 @@
 #include <ctype.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+
 
 //+
 // File:	shell.c
@@ -29,9 +31,11 @@
 //		if the command is not recognized an error is printed.
 //-
 
+// Maximum buffer size for command and maximum arguments allowed
 #define CMD_BUFFSIZE 1024
 #define MAXARGS 10
 
+// Function prototypes
 int splitCommandLine(char * commandBuffer, char* args[], int maxargs);
 int doInternalCommand(char * args[], int nargs);
 int doProgram(char * args[], int nargs);
@@ -48,17 +52,19 @@ int doProgram(char * args[], int nargs);
 //-
 
 int main() {
-
+    // Initialize variables
     char commandBuffer[CMD_BUFFSIZE];
     // note the plus one, allows for an extra null
     char *args[MAXARGS+1];
+    int nargs;
 
     // print prompt.. fflush is needed because
     // stdout is line buffered, and won't
     // write to terminal until newline
+
+    // Main command loop
     printf("%%> ");
     fflush(stdout);
-
     while(fgets(commandBuffer,CMD_BUFFSIZE,stdin) != NULL){
         //printf("%s",commandBuffer);
 
@@ -70,13 +76,19 @@ int main() {
             //printf("<%s>\n",commandBuffer);
 	}
 
-	// split command line into words.(Step 2)
+    // Split the command line into arguments
+    nargs = splitCommandLine(commandBuffer, args, MAXARGS);
 
-	// TODO
-
-	// add a null to end of array (Step 2)
-
-	// TODO
+    // Check if command is internal or external
+    if (nargs > 0) {
+             int internalCommandStatus = doInternalCommand(args, nargs); // Step 3
+        if (internalCommandStatus == 0) {
+            int programStatus = doProgram(args, nargs);
+        if (programStatus == 0) {
+            printf("Command not found.\n");
+        }
+    }
+        }
 
 	// debugging
 	//printf("%d\n", nargs);
@@ -86,15 +98,6 @@ int main() {
 	//}
 	// element just past nargs
 	//printf("%d: %x\n",i, args[i]);
-
-        // TODO: check if 1 or more args (Step 3)
-        
-        // TODO: if one or more args, call doInternalCommand  (Step 3)
-        
-        // TODO: if doInternalCommand returns 0, call doProgram  (Step 4)
-        
-        // TODO: if doProgram returns 0, print error message (Step 3 & 4)
-        // that the command was not found.
 
 	// print prompt
 	printf("%%> ");
@@ -121,29 +124,55 @@ int main() {
 //-
 
 char * skipChar(char * charPtr, char skip){
-    // TODO: contents of function
-    // TODO: replace null with proper value
-    return NULL;
+    while(*charPtr == skip && *charPtr != '\0') {
+        charPtr++;
+    }
+    return charPtr;
 }
 
 //+
-// Funtion:	splitCommandLine
+// Function: splitCommandLine
 //
-// Purpose:	TODO: give descritption of function
+// Purpose:	This function takes a command line input buffer and splits it
+//              into arguments. It then stores these arguments in an array for
+//              further processing.
 //
 // Parameters:
-//	TODO: parametrs and purpose
+//      commandBuffer - Pointer to the command line input buffer.
+//      args          - Array to store the individual arguments.
+//      maxargs       - Maximum number of arguments that can be parsed.
 //
-// Returns:	Number of arguments (< maxargs).
-//
+// Returns:	Number of arguments parsed and stored in the array.
 //-
 
+// Function to split the command line into arguments
 int splitCommandLine(char * commandBuffer, char* args[], int maxargs){
-   // TODO: Contents of function
+   int nargs = 0;
+
+   commandBuffer = skipChar(commandBuffer, ' ');
+
+    // Loop to break the command into tokens (arguments)
+   while (*commandBuffer != '\0' && nargs < maxargs) {
+    args[nargs] = commandBuffer;
     
-   // TODO: reutrn proper value
-   return 0;
+   
+    char *nextSpace = strchr(commandBuffer, ' ');
+    if (nextSpace != NULL) {
+        *nextSpace = '\0';
+        commandBuffer = nextSpace + 1;
+    } else {
+        commandBuffer = commandBuffer + strlen(commandBuffer);
+    }
+    
+    commandBuffer = skipChar(commandBuffer, ' ');
+    
+    nargs++;
 }
+
+    args[nargs] = NULL;
+    return nargs;
+}
+
 
 ////////////////////////////// External Program  (Note this is step 4, complete doeInternalCommand first!!) ///////////////////////////////////
 
@@ -156,25 +185,71 @@ char * path[] = {
 };
 
 //+
-// Funtion:	doProgram
+// Function: doProgram
 //
-// Purpose:	TODO: add description of funciton
+// Purpose:	This function takes an array of arguments and tries to execute
+//              the command specified in args[0]. It searches through the directories
+//              listed in the path array to find the executable.
 //
 // Parameters:
-//	TODO: add paramters and description
+//      args  - Array containing the command and its arguments.
+//      nargs - Number of arguments present in the array.
 //
-// Returns	int
+// Returns:	int
 //		1 = found and executed the file
 //		0 = could not find and execute the file
 //-
 
+// Function to execute external programs
 int doProgram(char * args[], int nargs){
-  // find the executable
-  // TODO: add body.
-  // Note this is step 4, complete doInternalCommand first!!!
 
+  char *cmd_path = NULL;
+  struct stat statbuf;
+  
+  // Extract just the command name from args[0]
+  char *command_name = strrchr(args[0], '/');
+  if (command_name) {
+    command_name++;  // Skip the '/'
+  } else {
+    command_name = args[0];
+  }
+
+    // Loop to check each directory in PATH for the command
+  for (int i = 0; path[i] != NULL; i++) {
+      
+    cmd_path = malloc(strlen(path[i]) + strlen(command_name) + 2);
+    sprintf(cmd_path, "%s/%s", path[i], command_name);
+        
+    printf("Checking path: %s\n", cmd_path);  // Debug print
+
+    if (stat(cmd_path, &statbuf) == 0) {
+      if (S_ISREG(statbuf.st_mode) && (statbuf.st_mode & S_IXUSR)) {
+        break;
+      }
+    }
+    free(cmd_path);
+    cmd_path = NULL;
+  }
+  
+  if (cmd_path == NULL) {
+    return 0;
+  }
+  
+  // Fork and execute the program
+  pid_t pid = fork();
+  if (pid == 0) {
+    execv(cmd_path, args);
+    exit(1);  
+  } else if (pid > 0) {
+    wait(NULL);  
+    free(cmd_path);
+  } else {
+    printf("Fork failed.\n");
+    return 0;
+  }
   return 1;
 }
+
 
 ////////////////////////////// Internal Command Handling (Step 3) ///////////////////////////////////
 
@@ -188,31 +263,48 @@ struct cmdStruct{
 };
 
 // prototypes for command handling functions
-// TODO: add prototype for each comamand function
+void exitFunc(char * args[], int nargs);
+void pwdFunc(char * args[], int nargs);
+void cdFunc(char * args[], int nargs);
+void lsFunc(char * args[], int nargs);
 
 // list commands and functions
 // must be terminated by {NULL, NULL} 
 // in a real shell, this would be a hashtable.
 struct cmdStruct commands[] = {
-   // TODO: add entry for each command
+   {"exit", exitFunc},
+   {"pwd", pwdFunc},
+   {"cd", cdFunc},
+   {"ls", lsFunc},
    { NULL, NULL}		// terminator
 };
 
 //+
 // Function:	doInternalCommand
 //
-// Purpose:	TODO: add description
+// Purpose:	This function takes an array of arguments and determines if
+//              the command specified in args[0] is an internal shell command.
+//              If it is, the function executes the corresponding command.
 //
 // Parameters:
-//	TODO: add parameter names and descriptions
+//      args  - Array containing the command and its arguments.
+//      nargs - Number of arguments present in the array.
 //
-// Returns	int
-//		1 = args[0] is an internal command
+// Returns:	int
+//		1 = args[0] is an internal command and was executed
 //		0 = args[0] is not an internal command
 //-
 
+// Function to check and execute internal commands
 int doInternalCommand(char * args[], int nargs){
-    // TODO: function contents (step 3)
+    int i = 0;
+    while (commands[i].cmdName != NULL) {
+        if (strcmp(commands[i].cmdName, args[0]) == 0) {
+            commands[i].cmdFunc(args, nargs);
+            return 1; 
+        }
+        i++;
+    }
     return 0;
 }
 
@@ -220,7 +312,44 @@ int doInternalCommand(char * args[], int nargs){
 // comand Handling Functions //
 ///////////////////////////////
 
-// TODO: a function for each command handling function
-// goes here. Also make sure a comment block prefaces
-// each of the command handling functions.
+// Function to handle 'exit' command
+void exitFunc(char * args[], int nargs) {
+    exit(0);
+}
+
+// Function to handle 'pwd' command
+void pwdFunc(char * args[], int nargs) {
+    char *cwd = getcwd(NULL, 0);
+    printf("%s\n", cwd);
+    free(cwd);
+}
+
+// Function to handle 'cd' command
+void cdFunc(char * args[], int nargs) {
+    if (nargs == 1) {
+        struct passwd *pw = getpwuid(getuid());
+        chdir(pw->pw_dir);
+    } else if (nargs == 2) {
+        if (chdir(args[1]) != 0) {
+            perror("chdir");
+        }
+    }
+}
+
+// Function to handle 'ls' command
+void lsFunc(char * args[], int nargs) {
+    struct dirent **namelist;
+    int n;
+
+    n = scandir(".", &namelist, NULL, NULL);
+    if (n < 0) {
+        perror("scandir");
+    } else {
+        while (n--) {
+            printf("%s\n", namelist[n]->d_name);
+            free(namelist[n]);
+        }
+        free(namelist);
+    }
+}
 
